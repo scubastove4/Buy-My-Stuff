d2b9f171716703191d79d5058bf04179cfd2734d
Steve Morello <smorello4@gmail.com>
2022-09-01 23:32:20 -0400

    test route working

--- buy_my_stuff_project/buy_my_stuff/views.py
+++ buy_my_stuff_project/buy_my_stuff/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.http import JsonResponse
 from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics
+from rest_framework import generics, status
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
@@ -27,3 +27,15 @@ def getRoutes(request):
     '/buy_my_stuff/token/refresh/'
   ]
   return Response(routes)
+
+@api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
+def testEndPoint(request):
+  if request.method == 'GET':
+    data = f"Congrats, {request.user}! The GET req worked!"
+    return Response({'response': data}, status=status.HTTP_200_OK)
+  elif request.method == 'POST':
+    text = request.POST.get('text')
+    data = f"Congrats, {request.user}! The POST req worked!"
+    return Response({'response': data}, status=status.HTTP_200_OK)
+  return Response({}, status.HTTP_400_BAD_Request)
