8bb17b652e54d383fec1d5d2463144146782e93d
Steve Morello <smorello4@gmail.com>
2022-09-06 09:41:52 -0400

    disable multi-word error

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,16 +4,19 @@
     <main>
       <router-view header="Buy My Stuff"></router-view>
     </main>
+    <Footer />
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 }
 </script>
---
+++ client/src/pages/ItemDetails.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'ItemDetails',
+  setup() {}
+}
+</script>
