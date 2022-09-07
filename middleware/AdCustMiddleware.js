eb9d7d6658d57bc573d7e1df00c6c3599c15d2d4
Steve Morello <smorello4@gmail.com>
2022-09-07 11:05:06 -0400

    add try catch to verifysessiontoken to verify across multiple secret keys

--- middleware/AdCustMiddleware.js
+++ middleware/AdCustMiddleware.js
@@ -4,18 +4,25 @@ require('dotenv').config()
 const ADMIN_APP_SECRET = process.env.ADMIN_APP_SECRET
 const CUSTOMER_APP_SECRET = process.env.CUSTOMER_APP_SECRET
 
-const verifySessionToken = (req, res, next) => {
+const verifySessionToken = async (req, res, next) => {
   const { token } = res.locals
-  let payload =
-    jwt.verify(token, ADMIN_APP_SECRET) ||
-    jwt.verify(token, CUSTOMER_APP_SECRET)
+  let payload
+  try {
+    payload = jwt.verify(token, ADMIN_APP_SECRET)
+  } catch (e) {
+    try {
+      payload = jwt.verify(token, CUSTOMER_APP_SECRET)
+    } catch (e) {
+      res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+    }
+  }
   if (payload) {
+    console.log(payload)
     res.locals.payload = payload
+    console.log(res.locals.payload)
     return next()
   }
-  res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
 }
-
 const stripToken = (req, res, next) => {
   try {
     const token = req.headers['authorization'].split(' ')[1]
