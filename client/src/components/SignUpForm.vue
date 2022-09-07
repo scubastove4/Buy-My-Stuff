7b400ece92e51fa9a58979817c23d9c18549e08e
Steve Morello <smorello4@gmail.com>
2022-09-06 22:40:36 -0400

    start sign up form

---
+++ client/src/components/SignUpForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <form @submit.prevent="" id="sign-up-form">
+    <span>
+      <label for="sign-up-name">First Name:</label>
+      <input
+        type="text"
+        id="sign-up-name"
+        name="firstName"
+        placeholder="Michael"
+        :value="signUpValues.firstName"
+        @input="
+          $emit('setSignUpFormValues', $event.target.name, $event.target.value)
+        "
+        required
+      />
+    </span>
+    <span>
+      <label for="sign-up-email">Email:</label>
+      <input
+        type="text"
+        id="sign-up-email"
+        name="email"
+        placeholder="michael@michael.com"
+        :value="signUpValues.email"
+        @input="
+          $emit('setSignUpFormValues', $event.target.name, $event.target.value)
+        "
+        required
+      />
+    </span>
+    <span>
+      <label for="sign-up-password">Password:</label>
+      <input
+        type="password"
+        id="sign-up-password"
+        name="password"
+        placeholder="Minimum 8 characters"
+        :value="signUpValues.password"
+        @input="
+          $emit('setSignUpFormValues', $event.target.name, $event.target.value)
+        "
+        minLength="8"
+        required
+      />
+    </span>
+    <span>
+      <label for="sign-up-confirm-password">Confirm Password:</label>
+      <input
+        type="password"
+        id="sign-up-confirm-password"
+        name="confirmPassword"
+        placeholder="Must match"
+        :value="signUpValues.confirmPassword"
+        @input="
+          $emit('setSignUpFormValues', $event.target.name, $event.target.value)
+        "
+        required
+      />
+    </span>
+    <button
+      type="submit"
+      :disabled="
+        !signUpValues.name ||
+        !signUpValues.age ||
+        !signUpValues.location ||
+        !signUpValues.username ||
+        !signUpValues.email ||
+        !signUpValues.password ||
+        !signUpValues.confirmPassword ||
+        signUpValues.password !== signUpValues.confirmPassword
+      "
+    >
+      Sign Up
+    </button>
+  </form>
+</template>
+
+<script setup></script>
--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
+    <SignUpForm v-if="upOrIn" />
     <LoginForm
+      v-else
       :loginValues="loginValues"
       @setLoginValues="setLoginValues"
       @login="login"
     />
+    <button id="up-or-in" @click="setUpOrIn">{{ upOrInText }}</button>
   </div>
 </template>
 
 <script setup>
 import { ref, defineEmits } from 'vue'
 import { useRouter } from 'vue-router'
+import SignUpForm from '../components/SignUpForm.vue'
 import LoginForm from '../components/LoginForm.vue'
 import { LoginCustomer } from '../services/AuthReq'
 
@@ -22,8 +26,18 @@ const loginValues = ref({
   email: '',
   password: ''
 })
+const upOrIn = ref(true)
+const upOrInText = ref('Cllick here to login')
 
-// const upOrIn = ref()
+function setUpOrIn() {
+  if (upOrIn.value) {
+    upOrIn.value = false
+    upOrInText.value = 'Click here to sign up'
+  } else {
+    upOrIn.value = true
+    upOrInText.value = 'Click here to login'
+  }
+}
 
 function setLoginValues(name, val) {
   loginValues.value[name] = val
@@ -45,3 +59,10 @@ async function login() {
   router.push('/')
 }
 </script>
+
+<style>
+#up-or-in {
+  width: 15%;
+  height: 15%;
+}
+</style>
