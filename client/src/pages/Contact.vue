63cb272bf0298daf5624f3e7cd0ec3e919d72ac1
Steve Morello <smorello4@gmail.com>
2022-09-11 15:44:54 -0400

    add contact form/service to contact page

--- client/src/pages/Contact.vue
+++ client/src/pages/Contact.vue
@@ -1,10 +1,64 @@
 <template>
-  <div></div>
+  <form @submit.prevent="submitEmail">
+    <label for="contact-name">Name: </label>
+    <input
+      type="text"
+      name="name"
+      id="contact-name"
+      :value="contactFormValues.name"
+      @input="setContactFormValues"
+      required
+    />
+    <label for="contact-email">Email: </label>
+    <input
+      type="email"
+      name="email"
+      id="contact-email"
+      :value="contactFormValues.email"
+      @input="setContactFormValues"
+      required
+    />
+    <label for="contact-message">Inquiry: </label>
+    <textarea
+      name="message"
+      id="contact-message"
+      :value="contactFormValues.message"
+      @input="setContactFormValues"
+      required
+    ></textarea>
+    <button
+      type="submit"
+      :disabled="
+        !contactFormValues.name ||
+        !contactFormValues.email ||
+        !contactFormValues.message
+      "
+    >
+      Send Email
+    </button>
+  </form>
 </template>
 
-<script>
-export default {
-  name: 'Contact',
-  setup() {}
+<script setup>
+import { ref } from 'vue'
+import emailjs from '@emailjs/browser'
+
+const serviceId = process.env.VUE_APP_SERVICE_ID
+const templateId = process.env.VUE_APP_TEMPLATE_ID
+const publicKey = process.env.VUE_APP_PUBLIC_KEY
+
+const contactFormValues = ref({
+  name: '',
+  email: '',
+  message: ''
+})
+
+function setContactFormValues(e) {
+  contactFormValues.value[e.target.name] = e.target.value
+}
+
+function submitEmail() {
+  emailjs.send(serviceId, templateId, contactFormValues.value, publicKey)
+  contactFormValues.value = { name: '', email: '', message: '' }
 }
 </script>
