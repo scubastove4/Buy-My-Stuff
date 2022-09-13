8b2b7c201a08333348b3c4b0dde889a0d0776d95
Steve Morello <smorello4@gmail.com>
2022-09-13 18:42:32 -0400

    try cors origin *

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
 
-const whitelist = [
-  'http://localhost:8080',
-  'https://buy-my-stuff-online.netlify.app'
-]
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const whitelist = [
+//   'http://localhost:8080',
+//   'https://buy-my-stuff-online.netlify.app'
+// ]
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.options('*', cors())
-app.use(cors(corsOptions))
+// app.options('*', cors())
+app.use(cors({ origin: '*' }))
 app.use(logger('dev'))
 app.use(express.json())
 
