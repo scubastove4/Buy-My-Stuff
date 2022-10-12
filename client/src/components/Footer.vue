eec9ce52d1d672a165a31f7f85e3ee41ae4d18b3
Steve Morello <smorello4@gmail.com>
2022-10-12 11:47:28 -0400

    start styling for 1000px width screen. import oh vue icons

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,6 +1,16 @@
 <template>
   <footer>
-    <router-link to="/contact" name="Contact">Contact</router-link>
+    <router-link to="/contact" name="Contact">
+      <v-icon id="email-logo" name="md-email-outlined" />
+      <!-- scale="1.75" -->
+    </router-link>
+    <a
+      href="https://www.instagram.com/scubastove4/"
+      target="_blank"
+      rel="noopener noreferrer"
+      ><v-icon id="ig-logo" name="io-logo-instagram"
+    /></a>
+    <!-- scale="1.5" -->
   </footer>
 </template>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { MdEmailOutlined, IoLogoInstagram } from 'oh-vue-icons/icons'
 import './styles/indexMain.css'
 import './styles/App.css'
 
-createApp(App).use(router).mount('#app')
+addIcons(MdEmailOutlined, IoLogoInstagram)
+
+createApp(App).use(router).component('v-icon', OhVueIcon).mount('#app')
